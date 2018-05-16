c994ec1048f2aecf16b6547c56a0dd84b18f9113
ethan randolph <ethanrandolph@cis.vvc.edu>
2018-05-15 17:14:37 -0700

    Three Button Update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,14 +4,53 @@
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
 {
-    button1 = new QPushButton("Click Me",this);
-    label1 = new QLabel("Label 1",this);
+    button1 = new QPushButton("Button 1",this);
+    button2 = new QPushButton("Button 2",this);
+    button3 = new QPushButton("Button 3",this);
+
+    label1 = new QLabel("Off",this);
+    label2 = new QLabel("Off",this);
+    label3 = new QLabel("Off",this);
+
+    button1->setCheckable(true);
+    button2->setCheckable(true);
+    button3->setCheckable(true);
+
     label1->move(button1->pos()+QPoint(button1->width()+5,0));
-    connect(button1,SIGNAL(clicked(bool)),this,SLOT(onbutton1Press()));
+    button2->move(button1->pos()+QPoint(0,button1->height()+5));
+    label2->move(button2->pos()+QPoint(button2->width()+5,0));
+    button3->move(button2->pos()+QPoint(0,button2->height()+5));
+    label3->move(button3->pos()+QPoint(button3->width()+5,0));
+
+    connect(button1,SIGNAL(toggled(bool)),this,SLOT(onbutton1Press(bool)));
+    connect(button2,SIGNAL(toggled(bool)),this,SLOT(onbutton2Press(bool)));
+    connect(button3,SIGNAL(toggled(bool)),this,SLOT(onbutton3Press(bool)));
 }
 
-void MainWindow::onbutton1Press()
+void MainWindow::onbutton1Press(bool toggled)
 {
-    qDebug() << "Pressed";
+    qDebug() << toggled;
+    if (toggled)
+        label1->setText("On");
+    else
+        label1->setText("Off");
+}
+
+void MainWindow::onbutton2Press(bool toggled)
+{
+    qDebug() << toggled;
+    if (toggled)
+        label2->setText("On");
+    else
+        label2->setText("Off");
+}
+
+void MainWindow::onbutton3Press(bool toggled)
+{
+    qDebug() << toggled;
+    if (toggled)
+        label3->setText("On");
+    else
+        label3->setText("Off");
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -14,11 +14,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
 
 public slots:
-    void onbutton1Press();
+    void onbutton1Press(bool toggled);
+    void onbutton2Press(bool toggled);
+    void onbutton3Press(bool toggled);
 
 private:
     QPushButton *button1;
     QLabel *label1;
+    QPushButton *button2;
+    QLabel *label2;
+    QPushButton *button3;
+    QLabel *label3;
 
 };
 
